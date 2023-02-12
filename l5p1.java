c33803e0fa37e560b8fad14d931ba83578984051
Mohammed Numaan <mdnumaan74234@gmail.com>
2023-02-12 22:44:43 +0300

    Pushing l5p1.java

---
+++ l5p1.java
@@ -0,0 +1,11 @@
+public class JenkinsWithGit {
+
+	public static void main(String[] args){				//code to print Unicode characters
+int 		charPerLine=0;
+for (int i = 33; i < 256; i++, 		charPerLinert)
+
+{
+System.out-print((char)1+((charPerLineR10=-0)?"In*:*It"'));
+System.out.print();
+}
+}
